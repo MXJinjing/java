819466f7b5e4fa07be060d3f1974fb611fcdac26
MXJinjing <2352695206@qq.com>
2022-10-28 13:47:58 +0800

    new note

---
+++ Classcompare/ObjectCom.java
@@ -0,0 +1,29 @@
+class Cylinder {
+    private static double pi = 3.14;
+    private double radius;
+    private int height;
+
+    public Cylinder(double r, int h) {
+        radius = r;
+        height = h;
+    }
+
+    public void compare(Cylinder v) {
+        if (this == v)
+            System.out.println("这两个对象相等");
+        else
+            System.out.println("这两个对象不相等");
+    }
+}
+
+class ObjectCom {
+    public static void main(String[] args) {
+        Cylinder v1 = new Cylinder(2.0, 3);
+        Cylinder v2 = new Cylinder(2.0, 3);
+        Cylinder v3 = v1;
+        // v1 v2 have different addresses
+        // v1 v3 point to the same address
+        v1.compare(v2);
+        v1.compare(v3);
+    }
+}
