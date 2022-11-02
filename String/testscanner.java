2af3d3a105ade53f6fff54730928ed3774bad31f
MXJinjing <2352695206@qq.com>
2022-11-02 22:51:07 +0800

    没卵用

--- String/testscanner.java
+++ String/testscanner.java
@@ -1,7 +1,12 @@
+import java.nio.charset.Charset;
 import java.util.Scanner;
 
 public class testscanner {
     public static void main(String[] args) {
+         //查看当前系统的字符编码方式
+         System.out.println(Charset.defaultCharset().name());
+         //查看当前系统的编码方式
+         System.out.println(System.getProperty("file.encoding"));
         System.out.println("测试输入");
         Scanner input = new Scanner(System.in);
         String st = input.next();
